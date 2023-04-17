1baf9331cd75ee649ed00e306166778cdeebccad
Irina Aleksandrova <myr-irina2@yandex.ru>
2023-04-17 19:04:42 +0300

    feat: add cash flow folder

---
+++ src/components/cash-flow/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import data from './../../json/cash-flow-general-plan.json';
+
+function CashFlowPlan() {
+  return <div>CashFlowPlan</div>;
+}
+
+export default CashFlowPlan;
