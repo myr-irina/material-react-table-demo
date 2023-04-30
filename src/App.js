33ddb26485734324aafeaa54c23ca87007f8da6f
Irina <myr-irina2@yandex.ru>
2023-04-30 11:41:36 +0300

    feat: add bdr plan by project page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import EmployeesByprojectFact from './components/employees/employees-by-project/
 import IncomeCostTotalsPlan from './components/income-cost/income-cost-totals-plan';
 import IncomeCostTotalsPlanSplit from './components/income-cost/income-cost-totals-plan-split';
 import IncomeCostTotalsFactSplit from './components/income-cost/income-cost-totals-fact-split';
+import IncomeCostPlanByProject from './components/income-cost/income-cost-plan-by-project';
 
 import CashFlowTotalsPlan from './components/cash-flow/cash-flow-plan/cash-flow-totals-plan';
 import CashFlowTotalsPlanSplit from './components/cash-flow/cash-flow-plan/cash-flow-totals-plan-split';
@@ -46,6 +47,10 @@ export default function App() {
             <Route
               path='/bdr-totals-fact-split'
               element={<IncomeCostTotalsFactSplit />}
+            />
+            <Route
+              path='/bdr-plan-by-project'
+              element={<IncomeCostPlanByProject />}
             />
 
             <Route path='/dds-totals-plan' element={<CashFlowTotalsPlan />} />
--- src/components/income-cost/income-cost-plan-by-project/index.jsx
+++ src/components/income-cost/income-cost-plan-by-project/index.jsx
@@ -1,7 +1,131 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
+import { Typography } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import { parseTableData2 } from '../../../utils/utils';
+
+import data from '../../../json/bdr-by-project-plan.json';
 
 function IncomeCostPlanByProject() {
-  return <div>IncomeCostPlanByProject</div>;
+  const TABLE_DATA = useMemo(() => parseTableData2(data), []);
+  // console.log({ TABLE_DATA });
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorFn: (data) => {
+          return data[0][0].projectType;
+        },
+        id: 'costType',
+        header: 'Название',
+      },
+      {
+        header: ' ',
+        size: 50,
+        id: 'costType1',
+      },
+      {
+        header: ' ',
+        size: 50,
+        id: 'costType2',
+      },
+      {
+        header: ' ',
+        size: 50,
+        id: 'costType3',
+      },
+      {
+        header: ' ',
+        size: 50,
+        id: 'costType4',
+      },
+    ],
+    []
+  );
+
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={TABLE_DATA}
+      renderDetailPanel={({ row }) => (
+        <TableContainer
+          sx={{
+            width: '100%',
+            margin: '0 auto',
+            overflowX: 'initial',
+          }}
+        >
+          {row.original.map((row) => {
+            if (row[0].projectName === 'amounts') return;
+            return (
+              <Table
+                stickyHeader
+                sx={{
+                  tableLayout: 'fixed',
+                  margin: '0 auto',
+                  width: '100%',
+                  '& .MuiTableCell-root:first-of-type': {
+                    width: '170px',
+                  },
+                }}
+                size='small'
+              >
+                <TableHead sx={{ overflow: 'hidden', whiteSpace: 'nowrap' }}>
+                  <TableRow>
+                    <TableCell>
+                      <Typography
+                        sx={{
+                          fontWeight: '700',
+                          fontSize: '14px',
+                          overflowX: 'hidden',
+                          whiteSpace: 'nowrap',
+                        }}
+                      >
+                        {row[0].projectName}
+                      </Typography>
+                    </TableCell>
+                    {Object.keys(row[0].value).map((cell) => (
+                      <TableCell component='th' key={cell}>
+                        <Typography
+                          sx={{
+                            fontWeight: '700',
+                            fontSize: '14px',
+                            overflowX: 'hidden',
+                            whiteSpace: 'nowrap',
+                            color: 'black',
+                          }}
+                        >
+                          {cell}
+                        </Typography>
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.map((tableRow) => {
+                    return (
+                      <TableRow>
+                        <TableCell>{tableRow.month}</TableCell>
+                        {Object.values(tableRow.value).map((val) => (
+                          <TableCell>{val}</TableCell>
+                        ))}
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            );
+          })}
+        </TableContainer>
+      )}
+    />
+  );
 }
 
 export default IncomeCostPlanByProject;
