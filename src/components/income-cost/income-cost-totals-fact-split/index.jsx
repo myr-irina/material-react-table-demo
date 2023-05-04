3d243961e94c50dc9c823c3e4105cec4bb455088
Irina <myr-irina2@yandex.ru>
2023-05-04 20:29:57 +0300

    fix: remove unnesassary json data

--- src/components/income-cost/income-cost-totals-fact-split/index.jsx
+++ src/components/income-cost/income-cost-totals-fact-split/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LayoutCollapsedTableBdr from '../../layouts-table/layout-collapsed-table-bdr';
-import data from '../../../json/income-cost-general-fact copy.json';
+import data from '../../../json/income-cost-general-fact.json';
 
 function IncomeCostTotalsFactSplit() {
   return <LayoutCollapsedTableBdr data={data} />;
